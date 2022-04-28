39d519e484e2f21b7bd79e0837ba31eca9765c11
Luniau <lubecarvalho@hotmail.com>
2022-04-28 16:56:09 +0200

    add icon component, made Icons animated

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react"; 
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
     return (
@@ -9,17 +10,16 @@ export default function WeatherInfo(props) {
                 <li>
                     <FormattedDate date={props.data.date}/>
                 </li>
-                <li className="text-capitalized">{props.data.description}</li>
+                <li className="text-capitalize">{props.data.description}</li>
             </ul>
             <div className="row mt-3">
                 <div className="col-6">
                     <div className="clearFix">
-                        <img
-                            src={props.data.icon}
-                            alt={props.data.description}
-                            className="float-left"
-                        />
-                        <span className="float-left">
+<div className="float-left">
+<WeatherIcon code={props.data.icon}  />
+                      
+  </div>
+                     <span className="float-left">
                             <span className="temperature">{Math.round(props.data.temperature)}</span>
                             <span className="unit"> °C</span>
                         </span>
